a19db6b2e13e78e600db8bd71aaa29e6e17d0914
JudahGabriel <judahgabriel@gmail.com>
2017-12-07 14:31:50 -0600

    Fixed formatting issue that caused CachedFingerprinted to throw.

--- src/ServiceWorker/Files/CacheFingerprinted.js
+++ src/ServiceWorker/Files/CacheFingerprinted.js
@@ -11,7 +11,7 @@
             .then(function (cache) {
                 return cache.addAll([
                     offlineUrl,
-                    { routes }
+                    {routes}
                 ]);
             });
     }
