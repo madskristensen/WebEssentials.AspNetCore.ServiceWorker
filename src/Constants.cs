abe6a2bbc13e2de75ee69e3b6f92599ef1ed74cf
Goran Pavlovic <goran.pavlovic@camis.com>
2018-07-03 10:25:51 -0400

    Added ability to configure a CSP nonce for service worker scripts via NWebSec tag helper

--- src/Constants.cs
+++ src/Constants.cs
@@ -7,5 +7,6 @@
         public const string DefaultCacheId = "v1.0";
         public const string WebManifestRoute = "~/manifest.webmanifest";
         public const string WebManifestFileName = "manifest.json";
+        public const string CspNonce = " nws-csp-add-nonce='true'";
     }
 }
