b66e75c1ea82b043d8f106aeec1b7a8361299998
Tiago Bernardo <hsbtiago@gmail.com>
2018-08-21 11:38:44 -0400

    Correction in the path os files they had ~ and not work in .net core 2.1 (#40)

--- src/Constants.cs
+++ src/Constants.cs
@@ -2,10 +2,10 @@
 {
     internal class Constants
     {
-        public const string ServiceworkerRoute = "~/serviceworker";
-        public const string Offlineroute = "~/offline.html";
+        public const string ServiceworkerRoute = "/serviceworker";
+        public const string Offlineroute = "/offline.html";
         public const string DefaultCacheId = "v1.0";
-        public const string WebManifestRoute = "~/manifest.webmanifest";
+        public const string WebManifestRoute = "/manifest.webmanifest";
         public const string WebManifestFileName = "manifest.json";
         public const string CspNonce = " nws-csp-add-nonce='true'";
     }
